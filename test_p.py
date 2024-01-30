d3be68eed3d8c7ed0dd928f2eef69ea23cbf156f
keerio <keerio@gmail.com>
2024-01-30 12:07:24 +0300

    bettr

--- test_p.py
+++ test_p.py
@@ -1,7 +1,6 @@
-#!/usr/local/bin/python3
-# read_xml.py
 import os
 import xml.etree.ElementTree as ET
+
 def read_gateway():
     tree = ET.parse('config.xml')
     root = tree.getroot()
@@ -13,30 +12,22 @@ def read_gateway():
             gateway = gateway_item.find('gateway').text
             name = gateway_item.find('name').text
             
-            # Write each item in the row followed by a comma
-            # Write a newline character at the end of each row
             f.write(f"{name},{gateway},{interface}\n")
 
-    # If the existing file is empty, simply rename the temporary file
     if not os.path.exists('options.txt') or os.stat('options.txt').st_size == 0:
         os.rename(temp_file, 'options.txt')
     else:
-        # Read the new lines from the temporary file
         with open(temp_file, 'r') as f:
             new_rows = [line.strip() for line in f]
 
-        # Read the existing rows from the output file
         with open('options.txt', 'r') as f:
-            existing_rows = {tuple(line.strip().split(',')) for line in f} # Strip the newline characters before splitting
+            existing_rows = [tuple(line.strip().split(',')) for line in f]
 
-        # Check if each new row is in the set of existing rows
-        # If it's not, add it to the list of rows to be written
         rows_to_be_written = []
         for new_row in new_rows:
             if tuple(new_row.split(',')) not in existing_rows:
                 rows_to_be_written.append(new_row)
 
-        # Write the rows to be written back to the output file
         with open('options.txt', 'a') as f:
             for row in rows_to_be_written:
                 f.write(row + '\n')
@@ -46,7 +37,6 @@ def display_menu(options):
     print("No. Name                 Gateway             Interface   Uptime Kuma URL")
     print("------------------------------------------------------------------------")
     for i, option in enumerate(options, start=1):
-        # Get the four values. If there's no fourth value, replace it with an empty string
         values = option[:4] + ['']*(4-len(option))
         print("{:2}. {:<20} {:<20} {:<10} {:<10}".format(i, *values))
 
@@ -69,7 +59,6 @@ def select_option(options):
                 options[selected_index].append(extra_value)
             return options[selected_index]
 
-
 if __name__ == "__main__":
     read_gateway()
 
@@ -81,7 +70,6 @@ if __name__ == "__main__":
             break
         print(f"\nYou have selected: {' '.join(map(str, selected_option))}")
         
-        # Write the updated options back to the file
         with open('options.txt', 'w') as file:
             for option in options:
                 file.write(','.join(option) + '\n')
